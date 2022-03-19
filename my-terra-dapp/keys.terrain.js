f482ffa291d3cb68b87492a2af3dc1118eeb9eb1
mohitdaswanii <57252279+mohitdaswanii@users.noreply.github.com>
2022-03-19 17:54:02 -0400

    update

---
+++ my-terra-dapp/keys.terrain.js
@@ -0,0 +1,12 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  testnet: {
+    mnemonic:
+      "cabin guide call gentle town frown clip radio profit merit fever tiger frown benefit lizard pyramid enter need always evidence please empty sauce frozen",
+  },
+  // custom_tester_2: {
+  //   privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  // },
+};
